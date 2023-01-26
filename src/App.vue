dfa70ece43541cb9bf4d727e09c3ce084ebf8a91
davfront <contact@david-pereira.net>
2023-01-26 12:23:29 +0100

    fix layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="layout">
     <div class="layout-body">
-      <RouterView :key="$route.path" />
+      <div class="layout-body-container">
+        <RouterView :key="$route.path" />
+      </div>
     </div>
     <div class="layout-footer">
       <RadioPlayer />
@@ -19,12 +21,16 @@ import RadioPlayer from '@/components/RadioPlayer.vue'
 .layout {
   display: flex;
   flex-direction: column;
+  min-height: 100vh;
   &-body {
     flex: 1 1 0;
     overflow: auto;
-    max-width: 40rem;
-    margin: 3rem auto 0;
-    padding: 2rem;
+    margin-top: 3rem;
+    &-container {
+      max-width: 40rem;
+      margin: 0 auto;
+      padding: 2rem;
+    }
   }
 }
 </style>
